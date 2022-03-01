ccb380774f58aa47ee2950c8fb2681045264fb29
shendu <e@onebook.club>
2022-03-01 16:25:18 +0800

    find_median_sorted_arrays

--- golang/main.go
+++ golang/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/golang-php-algorithm/golang/array"
+	"github.com/golang-php-algorithm/golang/test"
+)
+
 func main() {
 
 	//https://leetcode.com/problems/two-sum/
@@ -7,5 +12,11 @@ func main() {
 	//test.Dd(array.TwoSum(nums, 9))
 	//test.Dd(array.TwoSumV2(nums, 9))
 
-
+	//https://leetcode.com/problems/median-of-two-sorted-arrays/
+	var nums1 = []int{1, 3}
+	var nums2 = []int{2}
+	var nums3 = []int{1, 3}
+	var nums4 = []int{2, 4}
+	test.Dd(array.FindMedianSortedArrays(nums1, nums2))
+	test.Dd(array.FindMedianSortedArrays(nums3, nums4))
 }
